2481696e52aef35230fd476dd47d2c0ab83b8d92
Your JosephTseng <zxp930110@hotmail.com.tw>
2021-08-25 18:47:55 +0800

    modify wrapper

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, ReactElement } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -6,8 +6,8 @@ export class ChallengeInfo {
 	id: string;
 	title: string;
 	subTitle: string;
-	description: string;
-	constructor(id: string, title: string, subTitle: string, description: string) {
+	description: string | ReactElement;
+	constructor(id: string, title: string, subTitle: string, description: string | ReactElement) {
 		this.id = id;
 		this.title = title;
 		this.subTitle = subTitle;
@@ -16,7 +16,17 @@ export class ChallengeInfo {
 }
 
 const useStyle = makeStyles({
-	dailyChallengeWrapper: {},
+	dailyChallengeWrapper: {
+		'& a': {
+			color: '#136C72',
+			'&:hover': {
+				color: '#38CFD9',
+			},
+		},
+	},
+	content: {
+		paddingBlock: '1em',
+	},
 });
 
 export const DailyChallengeWrapper = (props: { info: ChallengeInfo; children: ReactNode }) => {
@@ -27,8 +37,12 @@ export const DailyChallengeWrapper = (props: { info: ChallengeInfo; children: Re
 		<div id={info.id} className={classes.dailyChallengeWrapper}>
 			<Typography variant={'h1'}>{info.title}</Typography>
 			<Typography variant={'h2'}>{info.subTitle}</Typography>
-			<Typography variant={'body1'}>{info.description}</Typography>
-			{children}
+			{typeof info.description == 'string' ? (
+				<Typography variant={'body1'}>{info.description}</Typography>
+			) : (
+				info.description
+			)}
+			<div className={classes.content}>{children}</div>
 		</div>
 	);
 };
